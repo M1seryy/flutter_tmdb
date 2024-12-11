1f77716526843c256e81b4638b16f1e3b6a4ba65
M1seryy <62029606+M1seryy@users.noreply.github.com>
2024-12-11 21:04:11 +0200

    cast screen

--- lib/widgets/details/screenCast.dart
+++ lib/widgets/details/screenCast.dart
@@ -5,6 +5,8 @@ class Screencast extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final castTextStyle = TextStyle(fontWeight: FontWeight.w600);
+
     return ColoredBox(
       color: Colors.white,
       child: Column(
@@ -18,38 +20,86 @@ class Screencast extends StatelessWidget {
             ),
           ),
           SizedBox(
-            height: 240,
-            child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: 20,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: DecoratedBox(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        border: Border.all(color: Colors.blueGrey),
-                      ),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(15),
-                        clipBehavior: Clip.hardEdge,
-                        child: Column(
-                          children: [
-                            Container(
-                              margin: EdgeInsets.only(
-                                  top: 1), //----------BAD PRACTICE
-                              width: 100,
-                              height: 100,
-                              child: Image(
-                                  image: NetworkImage(
-                                      'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQz8wN5zKhYp6mfWv_R7PJeKdR3gLB5PbFe1wTMZwxrG-ZHAhZXkLiNvmv5Zr3Gmd0QBW031BUrJIz6eHMDYCgFH9UBU45jVvsvEQaH0J0')),
+            height: 320,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Scrollbar(
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 20,
+                    itemExtent: 120,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            border: Border.all(
+                                width: 2,
+                                color: const Color.fromARGB(255, 0, 0, 0)),
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(15),
+                            clipBehavior: Clip.hardEdge,
+                            child: Column(
+                              children: [
+                                Container(
+                                  margin: EdgeInsets.only(
+                                      top: 2), //----------BAD PRACTICE
+                                  width: 100,
+                                  height: 100,
+                                  child: Image(
+                                      image: NetworkImage(
+                                          'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQz8wN5zKhYp6mfWv_R7PJeKdR3gLB5PbFe1wTMZwxrG-ZHAhZXkLiNvmv5Zr3Gmd0QBW031BUrJIz6eHMDYCgFH9UBU45jVvsvEQaH0J0')),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Column(
+                                    children: [
+                                      Text(
+                                          maxLines: 4,
+                                          "Steven Young",
+                                          style: castTextStyle),
+                                      SizedBox(
+                                        height: 7,
+                                      ),
+                                      Text(
+                                          maxLines: 4,
+                                          "Mark Grayson / Invictable (Voice)",
+                                          style: castTextStyle),
+                                      SizedBox(
+                                        height: 7,
+                                      ),
+                                      Text(
+                                          maxLines: 4,
+                                          "8 Episodes",
+                                          style: castTextStyle),
+                                      SizedBox(
+                                        height: 7,
+                                      ),
+                                    ],
+                                  ),
+                                )
+                              ],
                             ),
-                          ],
+                          ),
                         ),
-                      ),
-                    ),
-                  );
-                }),
+                      );
+                    }),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 30),
+            child: TextButton(
+                onPressed: null,
+                child: Text(
+                  "Full cast data",
+                  style: TextStyle(
+                      color: Colors.lightBlue,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400),
+                )),
           )
         ],
       ),
